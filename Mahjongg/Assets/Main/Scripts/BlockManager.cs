7c96d9df8fd4d2d3f4447381c8a44c0cb2b7ca32
Tim Wahrburg <t.wahrburg@gmail.com>
2024-02-23 16:51:24 +0100

    feat: Image-Loading works

--- Mahjongg/Assets/Main/Scripts/BlockManager.cs
+++ Mahjongg/Assets/Main/Scripts/BlockManager.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.Assertions;
 using System.Linq;
--- Mahjongg/Assets/Main/Scripts/SpriteManager.cs
+++ Mahjongg/Assets/Main/Scripts/SpriteManager.cs
@@ -1,4 +1,7 @@
+using Sirenix.Serialization;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -6,7 +9,27 @@ public class SpriteManager : MonoBehaviour
 {
     public List<(Sprite, Sprite)> LoadImages()
     {
-        Sprite[] sprites = Resources.LoadAll<Sprite>("Images");
+        string[] files = Directory.GetFiles($"{Application.streamingAssetsPath}/Images");
+
+        string[] filesNoMeta = files.Where(e => !e.EndsWith(".meta")).ToArray();
+
+        Texture2D[] textures = new Texture2D[filesNoMeta.Length];
+
+        for (int i = 0; i < filesNoMeta.Length; i++)
+        {
+            textures[i] = new Texture2D(1, 1);
+            byte[] bytes = File.ReadAllBytes(filesNoMeta[i]);
+            ImageConversion.LoadImage(textures[i], bytes);
+        }
+
+        Sprite[] sprites = new Sprite[textures.Length];
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            Rect rec = new Rect(0, 0, textures[i].width, textures[i].height);
+            sprites[i] = Sprite.Create(textures[i], rec, new Vector2(0.5f, 0.5f), 1);
+        }
+
         List<(Sprite, Sprite)> spriteList = new();
 
         for (int i = 0; i < sprites.Length; i+=2)
